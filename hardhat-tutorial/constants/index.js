7b0525b7e3da7130d17e831ab53b8af33bff129c
Bram Musuko <bram.musuko@gmail.com>
2022-05-06 15:49:54 +0700

    deploy nft contract

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xec08fb5C80d5E0536014c7B204C164595f0ead42";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
